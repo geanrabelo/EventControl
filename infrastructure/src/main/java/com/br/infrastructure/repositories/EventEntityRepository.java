0228c77931883053549fdf8a75dcd3fe9c4dca37
geanrabelo <geancardosorabelomota@gmail.com>
2025-05-06 11:21:25 -0300

    INFRATRUCTURE - EventEntityRepository criado

---
+++ infrastructure/src/main/java/com/br/infrastructure/repositories/EventEntityRepository.java
@@ -0,0 +1,9 @@
+package com.br.infrastructure.repositories;
+
+import com.br.infrastructure.domain.EventEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface EventEntityRepository extends JpaRepository<EventEntity, UUID> {
+}
