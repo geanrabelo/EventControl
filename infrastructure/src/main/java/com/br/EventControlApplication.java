8ae624b99e75900e527030f62bd08068a2f1c328
geanrabelo <geancardosorabelomota@gmail.com>
2025-05-06 11:12:08 -0300

    INFRASTRUCTURE - EventControlApplication criado

---
+++ infrastructure/src/main/java/com/br/EventControlApplication.java
@@ -0,0 +1,11 @@
+package com.br;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventControlApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EventControlApplication.class);
+    }
+}
