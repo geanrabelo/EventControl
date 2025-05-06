e62f41ab704c191ef2a93931e85acc34e15e65e4
geanrabelo <geancardosorabelomota@gmail.com>
2025-05-06 11:18:34 -0300

    INFRASTRUCTURE - GuestEntity criado e configurado

---
+++ infrastructure/src/main/java/com/br/infrastructure/domain/GuestEntity.java
@@ -0,0 +1,35 @@
+package com.br.infrastructure.domain;
+
+import com.br.core.enums.GuestStatus;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_guest")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@EqualsAndHashCode(of = "id")
+public class GuestEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    private String name;
+
+    private String email;
+
+    private String document;
+
+    @Enumerated(EnumType.STRING)
+    private GuestStatus guestStatus;
+
+    private UUID eventId;
+
+    private LocalDateTime registeredAt;
+}
