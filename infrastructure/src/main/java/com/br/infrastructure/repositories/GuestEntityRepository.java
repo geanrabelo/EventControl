a8e0ce5edfe60c55ade0edfbb019e655f8cd17e2
geanrabelo <geancardosorabelomota@gmail.com>
2025-05-06 11:22:07 -0300

    INFRASTRUCTURE - GuestEntityRepository criado

---
+++ infrastructure/src/main/java/com/br/infrastructure/repositories/GuestEntityRepository.java
@@ -0,0 +1,9 @@
+package com.br.infrastructure.repositories;
+
+import com.br.infrastructure.domain.GuestEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface GuestEntityRepository extends JpaRepository<GuestEntity, UUID> {
+}
