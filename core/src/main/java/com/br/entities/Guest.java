3fbdc3418aa6ff58e46b6fbd373e78fab2b5e6ce
geanrabelo <geancardosorabelomota@gmail.com>
2025-05-06 10:45:11 -0300

    CORE - GuestBuilder adiconado

--- core/src/main/java/com/br/entities/Guest.java
+++ core/src/main/java/com/br/entities/Guest.java
@@ -80,4 +80,65 @@ public class Guest {
         this.eventId = eventId;
         this.registeredAt = registeredAt;
     }
+
+    public static class GuestBuilder{
+        private UUID id;
+        private String name;
+        private String email;
+        private String document;
+        private GuestStatus guestStatus;
+        private UUID eventId;
+        private LocalDateTime registeredAt;
+
+        public GuestBuilder builder(){
+            return new GuestBuilder();
+        }
+
+        public GuestBuilder id(UUID id) {
+            this.id = id;
+            return this;
+        }
+
+        public GuestBuilder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public GuestBuilder email(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public GuestBuilder document(String document) {
+            this.document = document;
+            return this;
+        }
+
+        public GuestBuilder guestStatus(GuestStatus guestStatus) {
+            this.guestStatus = guestStatus;
+            return this;
+        }
+
+        public GuestBuilder eventId(UUID eventId) {
+            this.eventId = eventId;
+            return this;
+        }
+
+        public GuestBuilder registeredAt(LocalDateTime registeredAt) {
+            this.registeredAt = registeredAt;
+            return this;
+        }
+
+        public Guest build(){
+            return new Guest(
+                    this.id,
+                    this.name,
+                    this.email,
+                    this.document,
+                    this.guestStatus,
+                    this.eventId,
+                    this.registeredAt
+            );
+        }
+    }
 }
