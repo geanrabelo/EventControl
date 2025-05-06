e27cbba4b1650876ef223f5a8fbbb3066669db93
geanrabelo <geancardosorabelomota@gmail.com>
2025-05-06 10:57:19 -0300

    USECASES - EventUsecases create

---
+++ usecases/src/main/java/com/br/usecases/EventUsecases.java
@@ -0,0 +1,17 @@
+package com.br.usecases;
+
+import com.br.core.entities.Event;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface EventUsecases {
+
+    String createEvent(Event event);
+
+    List<Event> findAll();
+
+    Event findById(UUID id);
+
+    void canceledEvent(UUID id);
+}
