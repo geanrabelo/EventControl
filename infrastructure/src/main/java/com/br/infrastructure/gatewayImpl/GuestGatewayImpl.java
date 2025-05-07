7a714f339564d3839b59b9b9a0d997e828728223
geanrabelo <geancardosorabelomota@gmail.com>
2025-05-07 17:11:46 -0300

    INFRASTRUCTURE - GuestGatewayImpl alterando o método registeredGuest()

--- infrastructure/src/main/java/com/br/infrastructure/gatewayImpl/GuestGatewayImpl.java
+++ infrastructure/src/main/java/com/br/infrastructure/gatewayImpl/GuestGatewayImpl.java
@@ -3,12 +3,14 @@ package com.br.infrastructure.gatewayImpl;
 import com.br.application.gateway.GuestGateway;
 import com.br.core.entities.Guest;
 import com.br.core.enums.EnumCode;
+import com.br.core.exceptions.EventNotFound;
 import com.br.core.exceptions.GuestAlreadyRegisteredEvent;
 import com.br.core.exceptions.GuestNotFound;
 import com.br.infrastructure.domain.GuestEntity;
 import com.br.infrastructure.dto.guest.GuestEntityToGuest;
 import com.br.infrastructure.dto.guest.GuestToEntityJpa;
 import com.br.infrastructure.redis.GuestRedis;
+import com.br.infrastructure.repositories.EventEntityRepository;
 import com.br.infrastructure.repositories.GuestEntityRepository;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
@@ -22,26 +24,31 @@ import java.util.UUID;
 public class GuestGatewayImpl implements GuestGateway {
 
     private final GuestEntityRepository guestEntityRepository;
+    private final EventEntityRepository eventEntityRepository;
     private final RedisTemplate<String, GuestRedis> guestRedisTemplate;
     public static final String keyPattern = "guest: ";
 
 
-    public GuestGatewayImpl(GuestEntityRepository guestEntityRepository, RedisTemplate<String, GuestRedis> guestRedisTemplate){
+    public GuestGatewayImpl(GuestEntityRepository guestEntityRepository, RedisTemplate<String, GuestRedis> guestRedisTemplate, EventEntityRepository eventEntityRepository){
         this.guestEntityRepository = guestEntityRepository;
         this.guestRedisTemplate = guestRedisTemplate;
+        this.eventEntityRepository = eventEntityRepository;
     }
 
     @Override
     public UUID registeredGuest(Guest guest) {
-        if(!alreadyRegistered(guest)){
+        if(!alreadyRegistered(guest) && eventEntityRepository.existsById(guest.getEventId())){
             GuestEntity conversion = new GuestToEntityJpa(guest).toEntityJpa();
             conversion.setRegisteredAt(LocalDateTime.now());
             GuestEntity guestSaved = guestEntityRepository.save(conversion);
             GuestRedis guestRedis = GuestRedis.toGuestRedis(guestSaved);
             guestRedisTemplate.opsForZSet().add(keyPattern, guestRedis, guestRedis.id().hashCode());
             return guestSaved.getEventId();
+        } else if (alreadyRegistered(guest)) {
+            throw new GuestAlreadyRegisteredEvent(EnumCode.GU001.getMessage());
+        }else {
+            throw new EventNotFound(EnumCode.EV000.getMessage());
         }
-        throw new GuestAlreadyRegisteredEvent(EnumCode.GU001.getMessage());
     }
 
     @Override
@@ -102,6 +109,7 @@ public class GuestGatewayImpl implements GuestGateway {
     public void deleteById(UUID id) {
         if(existsById(id)){
             guestEntityRepository.deleteById(id);
+            guestRedisTemplate.opsForZSet().removeRangeByScore(keyPattern, id.hashCode(), id.hashCode());
         }else{
             throw new GuestNotFound(EnumCode.GU000.getMessage());
         }
